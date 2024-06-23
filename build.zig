4c542060c9aa4b6fe9cf4ea4af6a3278574f7c32
Karitham <kar@karitham.dev>
2024-06-23 11:28:21 +0000

    initial commit

---
+++ build.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) !void {
+    try zine.addWebsite(b, .{
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .static_dir_path = "static",
+        .host_url = "https://karitham.dev",
+        .title = "Kar's blog",
+    });
+}
---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+.title = "home",
+.author = "Karitham",
+.date = @date("2020-07-06T00:00:00"),
+.layout = "index.html",
+---
+# Kar's thoughts
+
+I'm using zine for this, hoping it's going to be good enough. Also [catpuccin](https://catppuccin.com)
