b41ebf4f4f11ecbc466a1d37584cba757503ed37
Karitham <kar@karitham.dev>
2024-09-13 10:05:14 +0200

    switch to mlugg/setup-zig@v1

--- flake.nix
+++ flake.nix
@@ -26,45 +26,12 @@
     flake-utils.lib.eachSystem systems (
       system: let
         pkgs = import nixpkgs {inherit overlays system;};
-        superHTML = pkgs.stdenv.mkDerivation rec {
-          name = "superhtml";
-          version = "0.4.3";
-          triplet = "x86_64-linux-musl";
-          src = pkgs.fetchurl {
-            url = "https://github.com/kristoff-it/superhtml/releases/download/v${version}/${triplet}.tar.gz";
-            sha256 = "sha256-PQg0NEM4nkLxQzAoZfNrCJ+RCTYqJfPRlWWVxAOFlpg=";
-          };
-          phases = ["unpackPhase" "installPhase"];
-          unpackPhase = "tar -xzf $src";
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ${triplet}/superhtml $out/bin/
-            chmod +x $out/bin/superhtml
-          '';
-        };
       in {
-        devShells.default = let
-          newPost = pkgs.writeShellScriptBin "new-post" ''
-            cat << EOF > content/posts/$1.md
-            ---
-            .title = "$1",
-            .date = @date("$(date '+%Y-%m-%dT%H:%M:%S')"),
-            .author = "Karitham",
-            .draft = true,
-            .layout = "post.shtml",
-            .tags = [],
-            ---
-            EOF
-          '';
-        in
-          pkgs.mkShell
-          {
-            nativeBuildInputs = with pkgs; [
-              zigpkgs.master
-              newPost
-              superHTML
-            ];
-          };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zigpkgs.master
+          ];
+        };
 
         # For compatibility with older versions of the `nix` binary
         devShell = self.devShells.${system}.default;
