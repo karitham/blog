77d94284e1b809139e01cfb3809fd69872b438d6
Karitham <kar@karitham.dev>
2024-09-25 16:26:33 +0000

    use local favicons instead of remote ones

--- build.zig
+++ build.zig
@@ -8,5 +8,14 @@ pub fn build(b: *std.Build) !void {
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .assets_dir_path = "assets",
+        .static_assets = &.{
+            "android-chrome-192x192.png",
+            "android-chrome-512x512.png",
+            "apple-touch-icon.png",
+            "favicon-16x16.png",
+            "favicon-32x32.png",
+            "favicon.ico",
+            "site.webmanifest",
+        },
     });
 }
